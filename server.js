613db4caf46daf2888949bb8606889be0a134576
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2024-02-22 10:57:25 +0100

    Farm.py

--- server.js
+++ server.js
@@ -3,8 +3,16 @@ const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
 
+const n_farms = 0;
+
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {
+    n_farms : n_farms
+  });
+});
+
+app.post('/add-farm', (req, res) => {
+  res.send('Farm added!');
 });
 
 app.listen(4000, () => console.log('Server listening on port 4000!'));
